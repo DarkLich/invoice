72e4c1849fe2c595d4691ee495dc3720895c9501
DarkLich <darkevillich@gmail.com>
2016-01-27 14:46:09 +0200

    last update

--- models/base.js
+++ models/base.js
@@ -9,12 +9,13 @@ var connection = mysql.createConnection({
 });
 
 var tarifs = [
-  {type: 'water', units: 'куб.м', printable: 1, system_id: '40946'},
-  {type: 'gas', units: 'куб.м', printable: 1, system_id: '56598'},
-  {type: 'electricity', units: 'кВт.час', printable: 1, system_id: '232215'},
-  {type: 'heat', units: 'мес', printable: 1, system_id: '31589'},
-  {type: 'communal', units: 'мес', printable: 1, system_id: '38149'},
-  {type: 'internet', units: 'мес', printable: 0, system_id: '5414801'}
+  {id: 1, name: 'water', type: 'counter' ,units: 'куб.м', printable: 1, system_id: '40946'},
+  {id: 2, name: 'gas', type: 'counter', units: 'куб.м', printable: 1, system_id: '56598'},
+  {id: 3, name: 'electricity', type: 'counter', units: 'кВт.час', printable: 1, system_id: '232215'},
+  {id: 4, name: 'heat', type: 'static', units: 'мес', printable: 1, system_id: '31589'},
+  {id: 5, name: 'communal', type: 'static', units: 'мес', printable: 1, system_id: '38149'},
+  {id: 6, name: 'internet', type: 'static', units: 'мес', printable: 0, system_id: '5414801'},
+  {id: 7, name: 'garage', type: 'static', units: 'мес', printable: 0, system_id: '35'}
 ];
 
 function objectToQuery(obj) {
@@ -29,7 +30,7 @@ function objectToQuery(obj) {
   return {keys: keyArray, vals: valArray}
 }
 
-connection.query('CREATE TABLE IF NOT EXISTS tariff_types (id INT(11) NOT NULL AUTO_INCREMENT, type VARCHAR(100), units VARCHAR(100), system_id VARCHAR(50), printable BOOL DEFAULT 0, PRIMARY KEY(id))', function(err, rows, fields) {
+connection.query('CREATE TABLE IF NOT EXISTS tariff_types (id INT(11) NOT NULL AUTO_INCREMENT, name VARCHAR(50), type VARCHAR(50), units VARCHAR(50), system_id VARCHAR(50), printable BOOL DEFAULT 0, PRIMARY KEY(id))', function(err, rows, fields) {
   if (err) throw err;
   console.log('The solution is111: ', rows , fields);
   fillTariffTypes();
@@ -47,22 +48,182 @@ function fillTariffTypes() {
       });
       console.log('query.sql', query.sql)
     }
-    connection.end();
+    //connection.end();
   });
 
 }
 
-var query = connection.query('CREATE TABLE IF NOT EXISTS tariffs (id INT(11) NOT NULL AUTO_INCREMENT, type_id INT(11), ' +
-    'created_at TIMESTAMP, value FLOAT, PRIMARY KEY(id))', function(err, rows, fields) {
+var query = connection.query('CREATE TABLE IF NOT EXISTS bills (id INT(11) NOT NULL AUTO_INCREMENT, tariff_id INT(11), ' +
+    'invoice_id INT(11), counter_prev FLOAT, counter_next FLOAT, tariff_rate FLOAT, tariff_value FLOAT, tariff2_rate FLOAT, tariff2_value FLOAT, PRIMARY KEY(id))', function(err, rows, fields) {
   console.log('query.sql', query.sql)
   if (err) throw err;
 
   console.log('The solution is: ', rows ,rows[0]);
 });
 
+var query = connection.query('CREATE TABLE IF NOT EXISTS invoices (id INT(11) NOT NULL AUTO_INCREMENT, ' +
+    'created_at TIMESTAMP, total FLOAT, PRIMARY KEY(id))', function(err, rows, fields) {
+  console.log('query.sql', query.sql)
+  if (err) throw err;
+  fillFirstInvoice()
+  console.log('The solution is: ', rows ,rows[0]);
+});
+
+function fillFirstInvoice() {
+  var invoice = {
+    total: 0
+  };
+  var bills = [
+    {
+      tariff_id: 1,
+      invoice_id: 1,
+      counter_prev: 64,
+      counter_next: 81,
+      tariff_rate: 9.684
+    },{
+      tariff_id: 2,
+      invoice_id: 1,
+      counter_prev: 1310,
+      counter_next: 1383,
+      tariff_rate: 7.188
+    },{
+      tariff_id: 3,
+      invoice_id: 1,
+      counter_prev: 6786,
+      counter_next: 7209,
+      tariff_rate: 0.456,
+      tariff_value: 100,
+      tariff2_rate: 0.789
+    },{
+      tariff_id: 4,
+      invoice_id: 1,
+      tariff_rate: 477.6
+    },{
+      tariff_id: 5,
+      invoice_id: 1,
+      tariff_rate: 50
+    },{
+      tariff_id: 6,
+      invoice_id: 1,
+      tariff_rate: 55
+    },{
+      tariff_id: 7,
+      invoice_id: 1,
+      tariff_rate: 0
+    }
+  ]
+  connection.query('SELECT COUNT(*) AS solution FROM invoices', function(err, rows, fields) {
+    if (err) throw err;
+    if (rows[0].solution === 0) {
+      var obj = objectToQuery(invoice);
+      var query = connection.query('INSERT INTO invoices (??) VALUES ?', [obj.keys, obj.vals], function(err, rows, fields) {
+        if (err) throw err;
+        console.log('The solution is: ', rows ,rows[0], fields);
+        _.each(bills, function(bill) {
+          var obj_bill = objectToQuery(bill);
+          var query = connection.query('INSERT INTO bills (??) VALUES ?', [obj_bill.keys, obj_bill.vals], function(err, rows, fields) {
+            if (err) throw err;
+            console.log('The solution is: ', rows ,rows[0], fields);
+            console.log('query.sql', query.sql)
+          });
+        });
+      });
+    }
+    //connection.end();
+  });
+
+}
+
 function addTariff(req, res, next) {
-  console.log('form', req)
+  var requiredFields = {
+    'gas.counter_prev': true,
+    'gas.counter_next': true,
+    'gas.tariff_rate': true,
+    'water.counter_prev': true,
+    'water.counter_next': true,
+    'water.tariff_rate': true,
+    'electricity.counter_prev': true,
+    'electricity.counter_next': true,
+    'electricity.tariff_rate': true,
+    'electricity.tariff_value': true,
+    'electricity.tariff2_rate': true,
+    'communal.tariff_rate': true,
+    'heat.tariff_rate': true
+  }
+
+  if (req.body && _.size(req.body) > 0) {
+    console.log(req.body)
+    var form = req.body
+    var form_bills = {}
+    var state = {};
+    _.each(form, function(val, key){
+      console.log ('654', key, requiredFields[key], requiredFields[key] && val === '', val)
+      if (requiredFields[key] && (val === '' || val === 'null')) {
+        if (val === '') {
+          state[key] = 'empty'
+        }
+        if (val === 'null') {
+          state[key] = 'null'
+        }
+        //throw new Error('oh no!');
+      } else {
+
+        var k = key.match(/^(\w+)\.(\w+)/);
+        console.log('k', k)
+        if (_.isUndefined(form_bills[k[1]])) form_bills[k[1]] = {}
+        form_bills[k[1]][k[2]] = val
+
+      }
+    })
+    if (_.isEmpty(state)) {
+      var obj = objectToQuery({total: 0});
+      var query = connection.query('INSERT INTO invoices (??) VALUES ?', [obj.keys, obj.vals], function (err, rows, fields) {
+        if (err) throw err;
+        var invoice_id = rows.insertId;
+        _.each(form_bills, function (bill, key) {
+          var tariff = _.find(tarifs, {name: key})
+          if (_.isUndefined(bill.tariff_id)) bill.tariff_id = tariff.id
+          if (_.isUndefined(bill.invoice_id)) bill.invoice_id = invoice_id
+        });
+        var new_bills = _.values(form_bills)
+        console.log(form_bills)
+        _.each(new_bills, function (bill) {
+          var obj_bill = objectToQuery(bill);
+          var query = connection.query('INSERT INTO bills (??) VALUES ?', [obj_bill.keys, obj_bill.vals], function (err, rows, fields) {
+            if (err) throw err;
+            console.log('The solution is: ', rows, rows[0], fields);
+            console.log('query.sql', query.sql)
+          });
+        });
+        next();
+      })
+    } else {
+      console.log('error', state);
+      throw new Error('Пустые поля');
+
+    }
+
+  }
+}
+
+function getLastTariff(req, res, next) {
+  connection.query('SELECT id FROM invoices ORDER BY id DESC LIMIT 1', function(err, rows, fields) {
+    if (err) throw err;
+    connection.query('SELECT * FROM bills LEFT JOIN tariff_types ON bills.tariff_id = tariff_types.id WHERE bills.invoice_id = ' + rows[0].id, function (err, rows, fields) {
+      if (err) throw err;
+      if (rows.length > 0) {
+        var lastBills = {};
+        _.each(rows, function (row) {
+          lastBills[row.name] = row
+        });
+        res.locals.lastBills = lastBills
+      }
+      next();
+    });
+    console.log('connected as id ' + connection.threadId);
+  });
 }
 
 module.exports = connection;
 module.exports.addTariff = addTariff;
+module.exports.getLastTariff = getLastTariff;
--- routes/invoice.js
+++ routes/invoice.js
@@ -5,12 +5,14 @@ var base = require('../models/base');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
   res.render('invoice_form', { title: 'Express' });
 });
 
-router.get('/create', function(req, res, next) {
-  console.log (req, res, next)
-  res.render('invoice_form', { title: 'Express' });
+router.get('/create', base.getLastTariff, function(req, res, next) {
+  //console.log (req, res, next)
+  console.log ('90909090', res.locals.lastBills)
+  res.render('invoice_form');
 });
 
 router.post('/create', base.addTariff, function (req, res, next) {
